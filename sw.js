9d10908ca30ccb3d21278e3e10d0d0e1904a81ef
wdsaidahh <said.afrizal88@gmail.com>
2025-12-07 21:43:36 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,53 @@
+const CACHE_NAME = "freespin-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+// Install Service Worker
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch Cache
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      // Jika ada di cache → pakai
+      if (response) return response;
+
+      // Jika tidak ada → ambil dari internet dan simpan cache
+      return fetch(event.request).then(fetchRes => {
+        return caches.open(CACHE_NAME).then(cache => {
+          if (event.request.url.startsWith("http")) {
+            cache.put(event.request, fetchRes.clone());
+          }
+          return fetchRes;
+        });
+      });
+    })
+  );
+});
+
+// Update SW
+self.addEventListener("activate", event => {
+  const whitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(keyList =>
+      Promise.all(
+        keyList.map(key => {
+          if (!whitelist.includes(key)) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
